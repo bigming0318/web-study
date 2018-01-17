33d147151d2c85351025ce871363d8a091bfa60d
13874393606@163.com <jm19980318>
2018-01-17 21:24:03 +0800

    section11 shopping --ok

--- JS/section5/newTest/newTest4/js/adver.js
+++ JS/section5/newTest/newTest4/js/adver.js
@@ -10,33 +10,4 @@ $(document).ready(function () {
         $(".arrowLeft").hide();
         $(".arrowRight").hide();
     })
-
-
-    var i = 1;
-    var j = 0;
-    $(".arrowRight").click(function () {
-        if (i < 6){
-            $(".adver").css("background","url('images/adver0"+(i+1)+".jpg')");
-            $("ul li").eq(j+1).css("background","orange");
-            $("ul li").eq(j+1).siblings().css("background","#333");
-            i += 1;
-            j += 1;
-        }else if (i = 6){
-            alert("这已经是最后一张图片了");
-        }
-        return i;
-    });
-
-    $(".arrowLeft").click(function () {
-        if (i != 1 || i <= 6){
-            $(".adver").css("background","url('images/adver0"+(i)+".jpg')");
-            $("ul li").eq(j).css("background","orange");
-            $("ul li").eq(j).siblings().css("background","#333");
-        } else if (i = 1){
-            $(".adver").css("background","url('images/adver0"+(i)+".jpg')");
-            $("ul li").eq(j).css("background","orange");
-            $("ul li").eq(j).siblings().css("background","#333");
-            alert("这已经是第一张图片了")
-        }
-    })
 });
